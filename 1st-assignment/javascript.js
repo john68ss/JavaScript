d774d91725460d3dca31d1d61825a2ae08f88a22
john68ss <endurobihorcountry@gmail.com>
2024-12-18 15:13:05 +0200

    modified:   1st-assignment/javascript.js

--- 1st-assignment/javascript.js
+++ 1st-assignment/javascript.js
@@ -39,3 +39,20 @@ console.log(lastLetter("abcd"));   // Output: "d"
 console.log(lastLetter("hello"));  // Output: "o"
 console.log(lastLetter("A"));      // Output: "A"
 console.log(lastLetter(""));       // Output: ""
+
+let answer = parseInt(prompt("Please enter a number you would like to FizzBuzz up to: "));
+
+for (let i = 1; i <= answer; i++){
+    if (i % 3 === 0 & i % 5 ===0){
+        console.log("FizzBuzz");
+    }
+    else if (i % 3 === 0) {
+        console.log("Fizz");
+    }
+    else if (i % 5 === 0) {
+        console.log("Buzz");
+    }
+    else {
+        console.log(i);
+    }
+}
